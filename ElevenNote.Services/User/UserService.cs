e62ffa1b00547ebc0b2c95142f5c0edce2ab5289
Katelyn Hedlund <katelyn.renee1013@gmail.com>
2022-12-15 21:38:46 -0500

    Updating UserServices - GetUserByIdAsync

--- ElevenNote.Services/User/UserService.cs
+++ ElevenNote.Services/User/UserService.cs
@@ -1,4 +1,4 @@
-using System.Threading.Tasks;
+using ElevenNote.Data;
 using ElevenNote.Data.Entities;
 using ElevenNote.Models.User;
 using Microsoft.AspNetCore.Identity;
@@ -33,6 +33,25 @@ namespace ElevenNote.Services.User
             entity.Password = passwordHasher.HashPassword(entity, model.Password);
         }
 
+            public async Task<UserDetail> GetUserByIdAsync(int userId)
+        {
+            var entity = await _context.Users.FindAsync(userId);
+            if (entity is null)
+                return null;
+            
+            var userDetail = new UserDetail
+            {
+                Id = entity.Id,
+                Email = entity.Email,
+                Username = entity.Username,
+                FirstName = entity.FirstName,
+                LastName = entity.LastName,
+                DateCreated = entity.DateCreated
+            };
+
+            return userDetail;
+        }
+
         private async Task<UserEntity> GetUserByEmailAsync(string email)
         {
         return await _context.Users.FirstOrDefaultAsync(user => user.Email.ToLower() == email.ToLower());
@@ -42,6 +61,7 @@ namespace ElevenNote.Services.User
         {
         return await _context.Users.FirstOrDefaultAsync(user => user.Username.ToLower() == username.ToLower());
         }
-
     }
+
+   
 }
